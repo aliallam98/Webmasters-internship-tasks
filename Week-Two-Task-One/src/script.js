dc057b5de6c7a917ee67efa0999ab6f4ceda77e6
aliallam98 <allam98.ali@gmail.com>
2024-07-09 20:38:08 +0300

    Finished landing section .

--- Week-Two-Task-One/src/script.js
+++ Week-Two-Task-One/src/script.js
@@ -4,3 +4,26 @@ const mobNav = document.getElementById("mobNav");
 menuButton.addEventListener("click", function () {
     mobNav.classList.toggle("hidden"); 
 });
+
+
+const counterElements = document.querySelectorAll(".counter-number");
+const animateCounters = () => {
+    counterElements.forEach((counterElement) => {
+      const targetNumber = parseInt(counterElement.textContent.replace("+", ""));
+      const duration = 3000;
+      let currentCount = 0;
+  
+      const animate = () => {
+        currentCount = Math.floor(currentCount + (targetNumber - currentCount) * (Math.min(window.performance.now() / duration, 1))); // Linear increment
+        counterElement.textContent = `${currentCount}+`;
+  
+        if (currentCount < targetNumber) {
+          requestAnimationFrame(animate);
+        }
+      };
+  
+      animate();
+    });
+  };
+window.addEventListener("load", animateCounters);
+
